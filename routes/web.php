b4fe3f5ff421a93c63e96acfe2322bae144d4936
Stefan Mermans <stefanmermans99@gmail.com>
2025-08-29 23:10:52 +0200

    Remove debug route

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-
-    Route::get('/debug', function (Request $request) {
-        return [
-            'headers' => $request->headers->all(),
-            'scheme' => $request->getScheme(),
-            'client_ip' => $request->getClientIp(),
-            'trusted_proxies' => $request->getTrustedProxies(),
-            'login' => route('login'),
-            'is_secure' => $request->isSecure(),
-            'server_https' => $_SERVER['HTTPS'] ?? 'not set',
-            'url_force_scheme' => config('app.url'),
-        ];
-    });
 });
 
 require __DIR__.'/settings.php';
