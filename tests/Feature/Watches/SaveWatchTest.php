8365b15806d60920e5c85c67e2fb943d05b73ed1
Stefan Mermans <stefanmermans99@gmail.com>
2026-03-30 19:32:41 +0200

    Cleanup

--- tests/Feature/Watches/SaveWatchTest.php
+++ tests/Feature/Watches/SaveWatchTest.php
@@ -227,9 +227,9 @@ describe('SaveWatch listener', function () {
     })->with('plex-events.scrobble.episode');
 
     it('does not create duplicate season', function (array $plexEvent) {
-       $plexEvent = PlexEventData::factory()
+        $plexEvent = PlexEventData::factory()
             ->alwaysValidate()
-           ->from(json_decode($plexEvent['payload'], true));
+            ->from(json_decode($plexEvent['payload'], true));
 
         dispatchScrobble($plexEvent, $this->user);
         dispatchScrobble($plexEvent, $this->user);
@@ -248,10 +248,10 @@ describe('SaveWatch listener', function () {
         $this->assertDatabaseCount(Watch::class, 1);
         $this->assertDatabaseCount(Season::class, 1);
         $this->assertDatabaseHas(Watch::class, [
-            'season_number' => 2
+            'season_number' => 2,
         ]);
         $this->assertDatabaseHas(Season::class, [
-            'season_number' => 2
+            'season_number' => 2,
         ]);
     })->with('plex-events.scrobble.episode.season-2');
 
@@ -265,10 +265,10 @@ describe('SaveWatch listener', function () {
         $this->assertDatabaseCount(Watch::class, 1);
         $this->assertDatabaseCount(Season::class, 1);
         $this->assertDatabaseHas(Watch::class, [
-            'season_number' => 3
+            'season_number' => 3,
         ]);
         $this->assertDatabaseHas(Season::class, [
-            'season_number' => 3
+            'season_number' => 3,
         ]);
     })->with('plex-events.scrobble.episode.season-3');
 });
